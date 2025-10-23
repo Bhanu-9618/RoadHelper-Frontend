ca750f1ed09ae142af9cb016b381a7450d804375
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-10-23 12:02:55 +0530

    Add Navbar component and setup routing

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-black">
+                <div className="container-fluid">
+                    <a className="navbar-brand" style={{ "fontWeight": "bold", "color": "white" }} href="#">RoadHelper</a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarScroll"
+                        aria-controls="navbarScroll"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarScroll">
+                        <ul
+                            className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+                            style={{ "--bs-scroll-height": "100px" }}
+                        >
+                            <li className="nav-item">
+                                <a className="nav-link active" style={{ "color": "white" }} aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" style={{ "color": "white" }} href="#">About Us</a>
+                            </li>
+                        </ul>
+                        <button className="btn btn-outline-light" type="button">SignUp</button>
+                        <button className="btn btn-outline-light" style={{ "marginLeft": "20px" }} type="button">SignIn</button>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
